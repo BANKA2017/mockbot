66c9759aaafca4c1cac7423f234cb2f985883922
BANKA2017 <kdwnil@hotmail.com>
2025-01-06 13:48:09 +0800

    fix: func FixMarkdownV2()

--- share/string.go
+++ share/string.go
@@ -3,5 +3,5 @@ package share
 import "regexp"
 
 func FixMarkdownV2(str string) string {
-	return regexp.MustCompile(`(?m)(_|\*|\[|\]|\(|\)|\~|\x60|\>|\#|\+|\-|\=|\||\{|\}|\.|\!)`).ReplaceAllString(str, "\\$1")
+	return regexp.MustCompile(`(?m)(\\_|\*|\[|\]|\(|\)|\~|\x60|\>|\#|\+|\-|\=|\||\{|\}|\.|\!)`).ReplaceAllString(str, "\\$1")
 }
